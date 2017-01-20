71d698ba6f1874237b2ca1701fb56e390d19dab4
anand <anand.jeyahar@gmail.com>
2017-01-20 14:27:42 +0530

    adding pomodoro

--- rc.lua
+++ rc.lua
@@ -7,7 +7,6 @@ package.path = config_path .. "/modules/?/init.lua;" .. package.path
 
 local math = require("math")
 local gears = require("gears")
-local pomodoro = require("pomodoro")
 
 -- local scratch = require("scratch")
 awful.client = require("awful.client")
@@ -50,7 +49,104 @@ awful.menu.menu_keys.exec = { "Return", "Space", }
 awful.menu.menu_keys.close = { "Escape", "BackSpace", }
 
 -- init pomodoro
-pomodoro.init()
+pomodoro = {}
+
+p = pomodoro
+
+-- quick convert to seconds
+local tosec = function(min) return min * 60 end
+
+-- in minutes settings for pomodoro. use tosec to convert to seconds
+p.conf = {
+    pause   = tosec(5),
+    work    = tosec(35),
+    limit   = tosec(60) -- limit for call
+}
+
+
+p.status  = {
+    true,
+    missing = p.conf.work
+}
+
+
+p.dialog = {
+    pause = {
+        title = "The break has finished.",
+        text = "Let's get that coding rolling!"
+    },
+
+    work = {
+        title = "Pomodoro has finished.",
+        test = "Time to take a break human!"
+    }
+}
+
+
+p.widget = wibox.widget.textbox()
+p.timer = timer {timeout = 1}
+
+
+function p:set_time(t)
+    if t >= self.conf.limit then
+        t = os.date("%X", t - self.conf.limit)
+    else
+        t = os.date("%M:%S", t)
+    end
+
+    self.widget:set_text(string.format(" Pomodoro:%s", t))
+end
+
+function p:notify(dialog, duration, status)
+    naughty.notify {
+        bg = "#ff0000",
+        fg = "#aaaaaa",
+        title = dialog.title,
+        text = dialog.text,
+        timeout = 10 --in seconds
+    }
+
+    self.status.missing = duration
+    self.status[1] = status
+
+    self.set_time(self, duration)
+    -- p:set_time(duration)
+end
+
+p:set_time(p.conf.work)
+p.widget:buttons(
+    awful.util.table.join(
+        awful.button({}, 1, function()
+            p.last = os.time()
+            p.timer:start()
+        end),
+        awful.button({}, 2, function()
+            p.timer:stop()
+        end),
+        awful.button({}, 3, function()
+            p.timer:stop()
+            p.status.missing = p.conf.work
+            p:set_time(p.conf.work)
+        end)
+    )
+)
+
+p.timer:connect_signal("timeout", function()
+    local now = os.time()
+    p.status.missing = p.status.missing - (now - p.last)
+    p.last = now
+
+    if p.status.missing > 0 then
+        p:set_time(p.status.missing)
+    else
+        if p.status[1] then
+            p:notify(p.dialog.work, p.conf.pause, false)
+        else
+            p:notify(p.dialog.pause, p.conf.work, true)
+        end
+        p.timer:stop()
+    end
+end)
 -- customization
 
 
@@ -1583,7 +1679,6 @@ menu = mymainmenu })
 --customization.widgets.textclock = wibox.widget.textbox()
 --bashets.register("date.sh", {widget=customization.widgets.textclock, update_time=1, format="$1 <span fgcolor='red'>$2</span> <small>$3$4</small> <b>$5<small>$6</small></b>"}) -- http://awesome.naquadah.org/wiki/Bashets
 
-customization.widgets.pomodoro = pomodoro.widget
 -- vicious widgets: http://awesome.naquadah.org/wiki/Vicious
 
 customization.widgets.bat0 = awful.widget.progressbar()
@@ -1803,7 +1898,6 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    right_layout.add(customization.widgets.pomodoro)
     if s == 1 then right_layout:add(wibox.widget.systray()) end
     -- right_layout:add(customization.widgets.cpuusage)
     -- right_layout:add(customization.widgets.memusage)
